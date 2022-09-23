703165d63ad568f4ac019bdad13b679882ef64d2
Michael Hoefert <mhoefert1@gmail.com>
2022-09-23 12:50:32 -0400

    Add psycopg and change around database to work on website

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-db.create_all()
+# db.create_all()
 
 
 class RegisterForm(FlaskForm):
