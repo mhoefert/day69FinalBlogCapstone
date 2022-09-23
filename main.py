9de33ca52e95d2e2d03bf5ca8f9175dd2faa366b
Michael Hoefert <mhoefert1@gmail.com>
2022-09-23 12:49:33 -0400

    Add psycopg and change around database to work on website

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -74,7 +74,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-# db.create_all()
+db.create_all()
 
 
 class RegisterForm(FlaskForm):
